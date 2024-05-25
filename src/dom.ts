a247d0f858235f03ed68fb63b6e704b3d179e101
Yappaholic <sav.boyar@gmail.com>
2024-05-25 11:01:56 +0300

    Added finally working data

--- src/dom.ts
+++ src/dom.ts
@@ -7,16 +7,22 @@ async function weatherCall(
   time: string,
   day: string | undefined,
 ) {
-  await getWeather(city, time, day).then((result) =>
-    displayCurrentWeather(result),
-  );
+  let result = await getWeather(city, time, day);
+  await new Promise((resolve, reject) => setTimeout(resolve, 100));
+  if (result[1].length > 3) {
+    displayCurrentWeather(result);
+  } else {
+    displayFutureWeather(result);
+  }
 }
+
 function clearContent(form: HTMLFormElement, button: HTMLButtonElement) {
   content!.removeChild(form);
   content!.removeChild(button);
 }
+
 function displayCurrentWeather(weather: any) {
-  const city = weather;
+  const city = weather[0] + ", " + weather[1];
   const temp = weather[2];
   const feel = weather[3];
   const cityText = document.createElement("h2");
@@ -28,6 +34,24 @@ function displayCurrentWeather(weather: any) {
   content!.appendChild(cityText);
   content!.appendChild(tempText);
   content!.appendChild(feelText);
+}
+function displayFutureWeather(weather: any) {
+  const cityText = document.createElement("h2");
+  cityText.textContent = weather[0];
+  content!.appendChild(cityText);
+  for (let i = 0; i < weather[1].length; i++) {
+    const div = document.createElement("div");
+    div.classList.toggle("forecast");
+    const date = document.createElement("p");
+    const temp = document.createElement("p");
+    date.classList.toggle("date");
+    temp.classList.toggle("temp");
+    date.textContent = weather[1][i].date;
+    temp.textContent = weather[1][i].temp;
+    div.appendChild(date);
+    div.appendChild(temp);
+    content!.appendChild(div);
+  }
 }
 select?.addEventListener("change", () => {
   if (select?.value === "Future") {
